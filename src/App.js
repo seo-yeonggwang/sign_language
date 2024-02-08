4b57f7fd2d1ef181bc79d9f1d21320da4675a4bf
seo-yeonggwang <dmb5627@naver.com>
2024-02-08 17:08:01 +0900

    라우터 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
 
 function App() {
   return (
-    <h1>Hi</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/quiz" element={<Quiz/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
